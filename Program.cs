6b9a23ea6950b02edafe7b8dfbeb7bc3e0f4269e
hieuld123 <hie.ld0210@gmail.com>
2026-04-06 14:51:40 +0700

    add lib

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using MyFirstProject;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("hieu");
+hieuCsharpLibrary test1 = new hieuCsharpLibrary();
+test1.test();
---
+++ hieuCsharpLibrary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class hieuCsharpLibrary
+    {
+        public void test()
+        {
+            Console.WriteLine("them file cs");
+        }
+    }
+}
